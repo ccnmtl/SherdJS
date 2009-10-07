eb8b92f1bde95d38ad6fe3a426882ac38e58ba88
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-10-07 16:09:43 +0000

    image inspiration if we ever get around to using openlayers
    git-svn-id: http://svn.ccnmtl.columbia.edu/sherdjs/branches/better@17322 1f418930-7ff8-0310-b8ad-c653122473bc

---
+++ src/image/openlayers/view.js
@@ -0,0 +1,9 @@
+/*
+goal: do something like:
+openlayers/openlayers/examples/image-layer.html
+but allow background image loading of higher resolutions
+
+Annotators:
+openlayers/openlayers/examples/modify-feature.html
+openlayers/openlayers/examples/vector-formats.html
+*/
