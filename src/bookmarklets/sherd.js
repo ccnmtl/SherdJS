2525bbcf0dd7f9cb0b0528d5d79c74737be49fd7
Schuyler Duveen <sky@skyb.us>
2010-01-15 12:24:33 -0500

    a bit of work for bookmarklet to also work as embeddable js

--- src/bookmarklets/sherd.js
+++ src/bookmarklets/sherd.js
@@ -2,28 +2,8 @@
   1. NO single quote characters
   2. NO // comments
 */
-var vietnam_update = function(obj) {
-    var hash = false;
-    var embs = document.getElementsByTagName("embed");
-    if (embs.length) {
-        var e = embs[0];
-        try{
-            e.Stop();
-            hash = "start="+Math.floor(e.GetTime()/e.GetTimeScale());
-        } finally{}
-    }
-    return {"html":$(".media").get(0),
-            "hash": hash||undefined,
-            "sources":{
-                "title":document.title,
-                "quicktime":$(".media").media("api").options.src,
-                "poster":$(".media img").get(0).src
-                /*,"thumb": XXXX */
-            }
-           };
-};
-
-var hosthandler = {
+MondrianBookmarklet = {
+  "hosthandler": {
     "youtube.com": {
         find:function(callback) {
             var video = document.getElementById("movie_player");
@@ -39,7 +19,7 @@ var hosthandler = {
                         }
                     }
                 }
-                var VIDEO_ID = video.getVideoUrl().match(/\?v=([^&]*)&/)[1];
+                var VIDEO_ID = video.getVideoUrl().match(/[?&]v=([^&]*)/)[1];
                 video.pauseVideo();
                 var obj = {
                     "html":video,
@@ -60,21 +40,46 @@ var hosthandler = {
         }
     },
     "vietnamwararchive.ccnmtl.columbia.edu": {
+        single:function() {
+            return (document.location.pathname.search("/record/display") == 0);
+        },
         find:function(callback) {
             var rv = [];
-            if (document.location.pathname.search("/record/display") == 0) 
-                rv = [ vietnam_update() ];
+            if (this.single()) 
+                rv = [ this.update() ];
             callback(rv);
         },
         decorate:function(objs) {
         },
-        update:vietnam_update
+        update:function(obj) {
+            var hash = false;
+            var embs = document.getElementsByTagName("embed");
+            if (embs.length) {
+                var e = embs[0];
+                try{
+                    e.Stop();
+                    hash = "start="+Math.floor(e.GetTime()/e.GetTimeScale());
+                } finally{}
+            }
+            return {"html":$(".media").get(0),
+                    "hash": hash||undefined,
+                    "sources":{
+                        "title":document.title,
+                        "quicktime":$(".media").media("api").options.src,
+                        "poster":$(".media img").get(0).src
+                        /*,"thumb": XXXX */
+                    }
+                   };
+        }
     },
     "digitaltibet.ccnmtl.columbia.edu": {
+        single:function() {
+            return (document.location.pathname.search("/image/") == 0);
+        },
         find:function(callback) {
             var real_site = "http://digitaltibet.ccnmtl.columbia.edu/";
             var img = jQuery(".node img").get(0);
-            if (!img || document.location.pathname.search("/image/") != 0) 
+            if (!img || !this.single()) 
                 return callback([]);            
 
             var images = ImageAnnotator.images[0].imager.images; /*annotationfield*/
@@ -139,18 +144,75 @@ var hosthandler = {
         decorate:function(objs) {
         }
     }
-};
-
-
-var gethosthandler = function() {
-    if (document.location.hostname in hosthandler) {
-        return hosthandler[document.location.hostname];
-    } else if (document.location.hostname.slice(4) in hosthandler) {
-        return hosthandler[document.location.hostname.slice(4)];
-    }
-};
-
-var obj2url = function(mondrian_url,obj) {
+  },/*hosthandler*/
+  "assethandler":{
+      "youtube": {
+          find:function(callback) {
+              callback([]);
+          }
+      },
+      "image": {
+          find:function(callback) {
+              var imgs = document.getElementsByTagName("img");
+              var result = [];
+              for (var i=0;i<imgs.length;i++) {
+                  if (imgs[i].width > 400 || imgs[i].height > 400) {
+                      result.push({
+                          "html":imgs[i],
+                          "sources": {
+                              "title":imgs[i].title || imgs[i].src,
+                              "image":imgs[i].src,
+                              "image-metadata":"w"+imgs[i].width+"h"+imgs[i].height,
+                          }
+                      });
+                  }
+              }
+              callback(result);
+          }
+      },
+      "mondrian": {
+          find:function(callback) {
+              var result = [];
+              if (String(document.body.innerHTML).indexOf("asset-links") < 0 ) {
+                  return callback(result);/*quick fail*/
+              }
+              var M = MondrianBookmarklet;
+              var divs = document.getElementsByTagName("div");
+              for (var i=0;i<divs.length;i++) {
+                  if (M.hasClass(divs[i]," asset-links ")) {
+                      var sources = {};
+                      var src_elems = divs[i].getElementsByTagName("a");
+                      for (var j=0;j<src_elems.length;j++) {
+                          if (M.hasClass(src_elems[j]," assetsource ")) {
+                              var src = src_elems[j];
+                              var reg = String(src.getAttribute("class")).match(/assetlabel-(\w+)/);
+                              if (reg != null) {
+                                  /*use getAttribute rather than href, to avoid urlencodings*/
+                                  sources[reg[1]] = src.getAttribute("href");
+                                  if (src.title) 
+                                      sources.title = src.title;
+                              }
+                          }
+                      }
+                      result.push({
+                          html:divs[i],
+                          sources:sources
+                      });
+                  }
+              }
+              return callback(result);
+          }
+      }
+  },/*assethandler*/
+  "gethosthandler":function() {
+      var hosthandler = MondrianBookmarklet.hosthandler;
+      if (document.location.hostname in hosthandler) {
+          return hosthandler[document.location.hostname];
+      } else if (document.location.hostname.slice(4) in hosthandler) {
+          return hosthandler[document.location.hostname.slice(4)];
+      }
+  },/*gethosthandler*/
+  "obj2url": function(mondrian_url,obj) {
     if (!obj.sources["url"]) obj.sources["url"] = document.location;
     var destination =  mondrian_url;
     for (a in obj.sources) {
@@ -160,33 +222,129 @@ var obj2url = function(mondrian_url,obj) {
         destination += "#"+obj.hash;
     }
     return destination;
-};
-
-var runners = {
+  },/*obj2url*/
+  "runners": {
     jump: function(mondrian_url,jump_now) {
         var final_url = mondrian_url;
-        var handler = gethosthandler();
+        var handler = MondrianBookmarklet.gethosthandler();
         if (!handler) {
             alert("Sorry, this website is not supported.");
             return;
         }
-        var my_callback = function(assets) {
+        var jump_with_first_asset = function(assets) {
             switch (assets.length) {
             case 0: 
                 return alert("This page does not contain an asset. Try going to an asset page.");
             case 1:
                 if (jump_now) {
-                    document.location = obj2url(mondrian_url, assets[0]);
+                    document.location = MondrianBookmarklet.obj2url(mondrian_url, assets[0]);
                 }
             }
         };
-        var assets = handler.find.call(handler, my_callback);
+        handler.find.call(handler, jump_with_first_asset);
     },
     decorate: function(mondrian_url) {
-
+        var M = MondrianBookmarklet;
+        function go() {
+            M.g = new M.Grabber(mondrian_url);
+        }
+        M.l = M.connect(window,"load",go);
     }
-};
+  },/*runners*/
+  "connect":function (dom,event,func) {
+      return ((dom.addEventListener)? dom.addEventListener(event,func,false) : dom.attachEvent("on"+event,func));
+  },/*connect*/
+  "hasClass":function (elem,cls) {
+      return (" " + (elem.className || elem.getAttribute("class")) + " ").indexOf(cls) > -1;
+  },
+  "Grabber" : function (mondrian_url, page_handler, options) {
+      this.options = {
+          tab_label:"Analyze in Mondrian",
+          target:document.body,
+          top:"100px",
+          side:"left",
+          fixed:true
+      }; if (options) for (a in options) {this.options[a]=options[a]};
+      var o = this.options;
+      var M = MondrianBookmarklet;
+      var self = this;
+      var comp = this.components = {};
+      comp.top = document.createElement("div");
+      comp.top.setAttribute("class","sherd-analyzer");
+      this.options.target.appendChild(comp.top);
+      comp.top.innerHTML = "<div class=\"sherd-tab\" style=\"display:block;position:absolute;"+o.side+":0px;z-index:9998;height:2.5em;top:"+o.top+";color:black;font-weight:bold;margin:0;padding:5px;border:3px solid black;text-align:center;background-color:#cccccc;text-decoration:underline;cursor:pointer;\">"+o.tab_label+"</div><div class=\"sherd-window\" style=\"display:none;position:absolute;z-index:9999;top:0;width:400px;height:400px;overflow:hidden;border:3px solid black;background-color:#cccccc\"><div class=\"sherd-window-inner\" style=\"overflow-y:auto;width:384px;height:390px;margin:1px;padding:0 6px 6px 6px;border:1px solid black;\"><button class=\"sherd-close\" style=\"float:right;\">close</button><h2>Assets on this Page</h2><p class=\"sherd-message\">Searching for assets....</p><ul></ul></div></div>";
+      comp.tab = comp.top.firstChild;
+      comp.window = comp.top.lastChild;
+      comp.ul = comp.top.getElementsByTagName("ul")[0];
+      comp.close = comp.top.getElementsByTagName("button")[0];
+      comp.message = comp.top.getElementsByTagName("p")[0];
+      this.onclick = function(evt) {
+          if (self.windowStatus) return;
+          self.windowStatus = true;
+          comp.window.style.display = "block";
+          comp.tab.style.display = "none";
+          self.findAssets();
+      };
+      M.connect(comp.tab, "click", this.onclick);
+      M.connect(comp.close, "click", function(evt) {
+          comp.window.style.display = "none";
+          comp.tab.style.display = "block";
+          self.windowStatus = false;
+      });
+      this.handler_count = 1;
+      this.assets_found = [];
+      this.findAssets = function() {
+          var handler = MondrianBookmarklet.gethosthandler();
+          if (handler) {
+              handler.find.call(handler, self.collectAssets);
+          } else {
+              handler = MondrianBookmarklet.assethandler;
+              for (h in MondrianBookmarklet.assethandler) {
+                  ++self.handler_count;
+                  handler[h].find.call(handler,self.collectAssets);
+              }
+              --self.handler_count;/*go to zero*/
+          }
+      };
+      this.collectAssets = function(assets) {
+          self.assets_found.push.apply(self.assets_found,assets);
+          --self.handler_count;
+          for (var i=0;i<assets.length;i++) {
+              self.displayAsset(assets[i]);
+          }
+          if (self.handler_count==0) {
+              self.finishedCollecting();
+          }
+      };
+      this.displayAsset = function(asset) {
+          var li = document.createElement("li");
+          var jump_url = M.obj2url(mondrian_url, asset);
+          var inner = asset.sources.title;
+          var img = asset.sources.thumb || asset.sources.image;
+          if (img) {
+              inner = "<img src=\""+img+"\" height=\"60px\" style=\"max-width:100px\" /> "+inner;
+          }
+          li.innerHTML = "<a href=\""+jump_url+"\">"+inner+"</a>";
+          comp.ul.appendChild(li);
+      };
+      this.finishedCollecting = function() {
+          comp.message.innerHTML = "";/*erase searching message*/
+          console.log(self.assets_found);
+          if (self.assets_found.length ==0) {
+              comp.ul.innerHTML = "<li>Sorry, no supported assets were found on this page. Try going to an asset page if you are on a list/search page.</li>";
+          }
+      };
+
+  }/*Grabber*/
+};/*MondrianBookmarklet (root)*/
+
 
 if (typeof mondrian_url == "string" && typeof mondrian_action == "string") {
-    runners[mondrian_action](mondrian_url,true);
+    MondrianBookmarklet.runners[mondrian_action](mondrian_url,true);
+} else {
+    var scripts = document.getElementsByTagName("script");
+    var me_embedded = scripts[scripts.length-1];
+    mondrian_url = String(me_embedded.src).split("/",3).join("/")+"/save/?";
+    mondrian_action = "decorate";
+    MondrianBookmarklet.runners[mondrian_action](mondrian_url,true);
 }
