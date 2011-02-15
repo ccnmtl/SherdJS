cbf4a7600300e457907206fc906d69641067e121
Schuyler Duveen <sky@skyb.us>
2011-02-15 15:59:23 -0500

    tinymce: include paste plugin for pasting from MS Word

--- scripts/update_tinymce.sh
+++ scripts/update_tinymce.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ../lib/tiny_mce3
 ant
-cat > jscripts/tinymce/plugins/searchreplace/editor_plugin.js jscripts/tiny_mce/plugins/table/editor_plugin.js jscripts/tiny_mce/plugins/inlinepopups/editor_plugin.js jscripts/tiny_mce/plugins/xhtmlxtras/editor_plugin.js > ../tiny_mce3_min.js
+cat jscripts/tiny_mce/tiny_mce_jquery.js jscripts/tiny_mce/plugins/paste/editor_plugin.js jscripts/tiny_mce/plugins/searchreplace/editor_plugin.js jscripts/tiny_mce/plugins/table/editor_plugin.js jscripts/tiny_mce/plugins/inlinepopups/editor_plugin.js jscripts/tiny_mce/plugins/xhtmlxtras/editor_plugin.js > ../tiny_mce3_min.js
