5e43339bacf5884f16a21d56a57a0419101d90d6
sdreher <sdreher@gmail.com>
2011-10-07 14:36:40 -0400

    A few more IE7 script bugs

--- src/video/views/flowplayer.js
+++ src/video/views/flowplayer.js
@@ -34,15 +34,15 @@ if (!Sherd.Video.Flowplayer && Sherd.Video.Base) {
         this.presentations = {
                 'small':{
                     width:function(){return 310;},
-                    height:function(){return 220;},
+                    height:function(){return 220;}
                 },
                 'medium': {
                     width:function(){return 540;},
-                    height:function(){return 383;},
+                    height:function(){return 383;}
                 },
                 'default': {
                     width:function(){return 620;},
-                    height:function(){return 440;},
+                    height:function(){return 440;}
                 }
         };
         
