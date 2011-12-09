495b762e4506880b488c98f1697c90bb1e8b2a5e
sdreher <sdreher@gmail.com>
2011-12-08 21:16:42 -0500

    post-commit

--- src/video/views/kaltura.js
+++ src/video/views/kaltura.js
@@ -11,8 +11,6 @@ if (!Sherd.Video.Kaltura && Sherd.Video.Base) {
         
         Sherd.Video.Base.apply(this,arguments); //inherit -- video.js -- base.js
         
-        console.log("testing pre-commit hook to minify");
-
         this.presentations = {
                 'small':{
                     width:function(){return 310;},
