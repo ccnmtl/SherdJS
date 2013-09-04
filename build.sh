ac493c7cdc3634a7a5832a1a4d4cea4fac01f7e0
Susan Dreher <sdreher@columbia.edu>
2013-09-04 10:09:32 -0400

    1.4 for develop

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.1.3.tar.gz
+tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.1.4.tar.gz
