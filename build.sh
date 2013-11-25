874fe2e57947c2947ec4bab301618fa30d54d881
Susan Dreher <sdreher@columbia.edu>
2013-11-25 09:17:32 -0500

    Incrementing version

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.2.4.tar.gz
+tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.2.5.tar.gz
