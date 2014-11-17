ef1bc209e97ed232ef2272d5df1743fd257f3068
Susan Dreher <sdreher@columbia.edu>
2014-11-17 13:08:55 -0500

    update version number.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.4.2.tar.gz
+tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.4.3.tar.gz
