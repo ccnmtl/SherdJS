fc7381d4cfdb6e0cba05eb20d9b7e7dc2ce4b901
Susan Dreher <sdreher@columbia.edu>
2015-03-02 19:54:31 -0500

    'archive' is a reserve word in MeTh. swap this out for 'metadata-photostream' to prevent conflicts.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.4.7.tar.gz
+tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.4.8.tar.gz
