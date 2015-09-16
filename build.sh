db8d720d7f389480702db3afd2ef306179316a2d
Nik Nyby <nnyby@columbia.edu>
2015-09-16 09:49:19 -0400

    Release version 0.5.6

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.5.5.tar.gz
+tar -c --exclude='.*' --exclude='build.sh' --exclude='package.json' -f - * | gzip > sherdjs-0.5.6.tar.gz
